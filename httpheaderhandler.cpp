35ce4f8ef7ee4214f7b805ed3064d2ca4a26dea4
Ran Bao <worksev@gmail.com>
2015-12-27 17:42:12 +1300

    Some random update

--- httpheaderhandler.cpp
+++ httpheaderhandler.cpp
@@ -4,15 +4,15 @@
 HTTPHeaderHandler::HTTPHeaderHandler(QByteArray &rawHeader)
 {
     // extract target
-    QList<QByteArray> request_field = rawHeader.mid(0, rawHeader.indexOf('\r')).split(' ');
+    QList<QByteArray> method_field = rawHeader.mid(0, rawHeader.indexOf('\r')).split(' ');
 
     // check if the header methods has been break into 3 piece
-    Q_ASSERT(request_field.length() == 3);
+    Q_ASSERT(method_field.length() == 3);
 
     // assign each element
-    request["Method"] = request_field.at(0);
-    request["Target"] = request_field.at(1);
-    request["Version"] = request_field.at(2);
+    method["Method"] = method_field.at(0);
+    method["Target"] = method_field.at(1);
+    method["Version"] = method_field.at(2);
 
     // process the rest of headers
     QList<QByteArray> header_field_list = rawHeader.mid(rawHeader.indexOf('\n') + 1).trimmed().split('\n');
@@ -26,8 +26,73 @@ HTTPHeaderHandler::HTTPHeaderHandler(QByteArray &rawHeader)
         QByteArray value = line.mid(loc + 1).trimmed();
         header[key] = value;
     }
+}
 
-    qDebug() << request;
-    qDebug() << header;
+void HTTPHeaderHandler::setHeaderField(QByteArray &key, QByteArray value)
+{
+    // check the length of each value
+    Q_ASSERT(value.length() < 8190);
+
+    // check the total number of fields
+    Q_ASSERT(header.size() < 100);
+
+    header[key] = value;
+}
+
+QByteArray HTTPHeaderHandler::getHeaderField(const QByteArray &key)
+{
+    return header[key];
+}
+
+HTTPHeaderHandler::KnownMethods HTTPHeaderHandler::getMethod()
+{
+    const QByteArray method_value = method["Method"];
+
+    if (method_value == "GET")
+    {
+        return GET;
+    }
+    else if (method_value == "HEAD")
+    {
+        return HEAD;
+    }
+    else if (method_value == "POST")
+    {
+        return POST;
+    }
+    else if (method_value == "PUT")
+    {
+        return PUT;
+    }
+    else if (method_value == "DELETE")
+    {
+        return DELETE;
+    }
+    else if (method_value == "CONNECT")
+    {
+        return CONNECT;
+    }
+    else if (method_value == "OPTIONS")
+    {
+        return OPTIONS;
+    }
+    else if (method_value == "TRACE")
+    {
+        return TRACE;
+    }
+    else
+    {
+        return UNKNOWN;
+    }
+}
+
+QByteArray HTTPHeaderHandler::getTarget()
+{
+    return method["Target"];
+}
+
+QByteArray HTTPHeaderHandler::getVersion()
+{
+    return method["Version"];
 }
 
--- httpheaderhandler.h
+++ httpheaderhandler.h
@@ -6,14 +6,32 @@
 
 class HTTPHeaderHandler
 {
+    enum KnownMethods
+    {
+        UNKNOWN = 0,
+        GET = 1,
+        HEAD = 2,
+        POST = 3,
+        PUT = 4,
+        DELETE = 5,
+        CONNECT = 6,
+        OPTIONS = 7,
+        TRACE = 8
+    };
+
+
 public:
     HTTPHeaderHandler(QByteArray &rawHeader);
-    void setValue(QByteArray &key, QByteArray value);
-    QByteArray getValue(QByteArray &key);
+    void setHeaderField(QByteArray &key, QByteArray value);
+    QByteArray getHeaderField(const QByteArray &key);
+
+    KnownMethods getMethod();
+    QByteArray getTarget();
+    QByteArray getVersion();
 
 protected:
     QMap<QByteArray, QByteArray> header;
-    QMap<QByteArray, QByteArray> request;
+    QMap<QByteArray, QByteArray> method;
 };
 
 #endif // HTTPHEADERHANDLER_H
